9e9b3ad44acbf82fad211a080dc3ba021a875f90
Sreenesh Reddy <hizahero@gmail.com>
2025-10-30 11:25:07 +0530

    Add project files.

---
+++ CentralizedExceptionHandling_BuiltInMiddleware/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CentralizedExceptionHandling_BuiltInMiddleware.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [Route("/error")]
+        public IActionResult HandleError()
+        {
+            var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            return Problem(
+                statusCode: StatusCodes.Status500InternalServerError,
+                title: "An Unexpected error has occured!"
+                );
+        }
+    }
+}
